7a82cad55e2a5a6566b4df73759ced08a796df28
ashraya.tuladhar <ashraya.tuladhar@tekvortex.com>
2024-10-23 11:15:30 +0545

    added pse container ip

--- src/com/invisirisk/IpTablesSetup.groovy
+++ src/com/invisirisk/IpTablesSetup.groovy
@@ -18,17 +18,18 @@ class IpTablesSetup {
     //     '''
     // }
     static void setup(script) {
-    script.sh """
-        def containerName = env.PSE_CONTAINER_NAME
+        def containerName = env.PSE_CONTAINER_NAME    
+        script.sh """
+        
         echo "Installing dependencies"
         apk add iptables ca-certificates git curl
         echo "Setting up iptables"
-        iptables -t nat -N ${env.PSE_CONTAINER_NAME}
-        iptables -t nat -A OUTPUT -j ${env.PSE_CONTAINER_NAME}
+        iptables -t nat -N ${containerName}
+        iptables -t nat -A OUTPUT -j ${containerName}
         echo "Setting up PSE"
-        PSE_IP=\$(getent hosts ${env.PSE_CONTAINER_NAME} | awk '{ print \$1 }')
+        PSE_IP=\$(getent hosts ${containerName} | awk '{ print \$1 }')
         echo "PSE_IP is \${PSE_IP}"
-        iptables -t nat -A ${env.PSE_CONTAINER_NAME} -p tcp -m tcp --dport 443 -j DNAT --to-destination \${PSE_IP}:12345
+        iptables -t nat -A ${containerName} -p tcp -m tcp --dport 443 -j DNAT --to-destination \${PSE_IP}:12345
     """
 }
 }
